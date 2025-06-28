46f79157034da8520404bf4b4e5421208ac07704
Siddami <dharmmy621@gmail.com>
2025-06-28 07:52:48 +0100

    feat: sort layout and made footer and navbar consistent and responsive

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Link from "next/link";
 import { Header } from "./components/Header";
 import { Oswald } from "next/font/google";
+import { Footer } from "./components/Footer";
 
 const oswald = Oswald({
   subsets: ["latin"],
@@ -28,33 +28,14 @@ export default function RootLayout({
           content="default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https: data:;"
         />
       </head>
-      <body className="text-white font-oswald h-screen">
-        <div className="container max-w-[1440px] mx-auto">
-          {/* Header */}
-          <Header />
+      <body className="text-white font-oswald min-h-screen bg-gradient-to-b from-[#020213] to-[#091C38] bg-no-repeat bg-cover">
+        {/* Header */}
+        <Header />
 
-          <main className="container mx-auto p-4">{children}</main>
-          {/* Footer */}
-          <footer className="bg-purple-900 p-4 mt-8 text-center">
-            <div className="space-x-4">
-              <Link href="/about" className="hover:text-white">
-                ABOUT
-              </Link>
-              <Link href="/contact" className="hover:text-white">
-                CONTACT
-              </Link>
-              <Link href="/blog" className="hover:text-white">
-                BLOG
-              </Link>
-              <Link href="/programming" className="hover:text-white">
-                PROGRAMMING
-              </Link>
-              <Link href="/submit" className="hover:text-white">
-                SUBMIT
-              </Link>
-            </div>
-          </footer>
-        </div>
+        <main className="container max-w-[1440px] mx-auto p-4">{children}</main>
+        
+        {/* Footer */}
+        <Footer />
       </body>
     </html>
   );
